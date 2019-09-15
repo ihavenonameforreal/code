d41e8c7259da9d432714a0d093387bfa60600ebc
nobody <root@localhost>
2019-09-15 21:25:52 +0200

    use readline

--- lib/code/application.rb
+++ lib/code/application.rb
@@ -1,12 +1,22 @@
 require 'execjs'
+require "readline"
 
 class Code
   class Application
+    VERBS = [
+      "donner", "afficher", "aide", "exit"
+    ].sort
+
+    COMPARE = proc { |verb| VERBS.grep(/^#{Regexp.escape(verb)}/) }
+
     def initialize(path)
       @path = path
       @name = @path.split('/').last
       @history_path = "#{@path}/history.data"
       @data_path = "#{@path}/#{@name}.data"
+
+      Readline.completion_append_character = " "
+      Readline.completion_proc = COMPARE
     end
 
     def self.start(path)
@@ -14,12 +24,15 @@ class Code
     end
 
     def start
-      loop do
-        print '> '
-        Code::Evaluator.eval(STDIN.gets.strip,
-          data_path: @data_path,
-          history_path: @history_path
-        )
+      while buffer = Readline.readline("> ", true)
+        begin
+          Code::Evaluator.eval(buffer,
+            data_path: @data_path,
+            history_path: @history_path
+          )
+        rescue ExecJS::ProgramError => error
+          puts error.message
+        end
       end
     end
   end
