ebe4eaf274d2c5a9f6996d8ef05a09d157b4a793
nobody <root@localhost>
2019-09-15 21:09:26 +0200

    journal, data, application works

--- lib/code/application.rb
+++ lib/code/application.rb
@@ -5,7 +5,7 @@ class Code
     def initialize(path)
       @path = path
       @name = @path.split('/').last
-      @journal_path = "#{@path}/#{@name}.journal"
+      @history_path = "#{@path}/history.data"
       @data_path = "#{@path}/#{@name}.data"
     end
 
@@ -18,7 +18,7 @@ class Code
         print '> '
         Code::Evaluator.eval(STDIN.gets.strip,
           data_path: @data_path,
-          journal_path: @journal_path
+          history_path: @history_path
         )
       end
     end
--- lib/code/evaluator.rb
+++ lib/code/evaluator.rb
@@ -2,30 +2,32 @@ require 'execjs'
 
 class Code
   class Evaluator
-    def initialize(text, data_path: nil, journal_path: nil)
+    def initialize(text, data_path: nil, history_path: nil)
       @text = text
       @parsed = Code::Parser.parse(text)
-      @data_path = data_path
-      @data = data_path ? Code::Data.load(read_file(data_path)) : {}
-      @journal_path = journal_path
-      @journal = journal_path ? Code::Data.load(read_file(journal_path)) : []
-    end
 
-    def read_file(filepath)
-      if File.exists?(filepath)
-        File.read(filepath)
-      else
-        File.write(filepath, "{}")
+      @data_path = data_path
+      @data = {}
+
+      if @data_path && File.exists?(@data_path)
+        @data = Code::Data.load(File.read(@data_path))
+      end
+
+      @history_path = history_path
+      @history = []
+
+      if @history_path && File.exists?(@history_path)
+        @history = Code::Data.load(File.read(@history_path))
       end
     end
 
-    def self.eval(text, data_path: nil, journal_path: nil)
-      new(text, data_path: data_path, journal_path: nil).eval
+    def self.eval(text, data_path: nil, history_path: nil)
+      new(text, data_path: data_path, history_path: history_path).eval
     end
 
     def eval
       @parsed.each.with_index do |line, index|
-        @journal << { line: line, time: Time.now, index: index }
+        @history << { line: line, time: Time.now, index: index }
 
         if line["verb"] == "donner"
           @data[line["name"]] ||= 0
@@ -36,8 +38,8 @@ class Code
           p line
           p @data
 
-          if @journal_path
-            File.write(@journal_path, Code::Data.dump(@journal))
+          if @history_path
+            File.write(@history_path, Code::Data.dump(@history))
           end
 
           if @data_path
@@ -48,8 +50,8 @@ class Code
         elsif line["verb"] == "help" || line["verb"] == "aide"
           puts <<~HELP
             donner MONTANT à PERSONNE (de la part de PERSONNE) (pour RAISON)
-            show
-            help
+            afficher
+            aide
             exit
           HELP
         elsif line["verb"] == "exit"
