de2a1e9080d63d7983c1e2bf39dca064b12dff52
nobody <root@localhost>
2019-09-18 13:05:56 +0200

    number translation in parser

--- lib/code/evaluator.rb
+++ lib/code/evaluator.rb
@@ -2,30 +2,6 @@ require 'execjs'
 
 class Code
   class Evaluator
-    LITTERALS = [
-      ["zero", "xéro", 0],
-      ["one", "un", "une", 1],
-      ["two", "deux", 2],
-      ["three", "trois", 3],
-      ["four", "quatre", 4],
-      ["five", "cinq", 5],
-      ["six", "six", 6],
-      ["seven", "sept", 7],
-      ["eight", "huit", 8],
-      ["nine", "neuf", 9],
-      ["ten", "dix", 10],
-      ["eleven", "onze", 11],
-      ["twelve", "douze", 12],
-      ["thirsteen", "trése", 13],
-      ["fourteen", "quatorze", 14],
-      ["fifthteen", "quize", 15],
-      ["sixteen", "seize", 16],
-      ["seventeen", "dix-sept", 17],
-      ["eighteen", "dix-huit", 18],
-      ["nineteen", "dix-neuf", 19],
-      ["twenty", "vingt", 20],
-    ]
-
     def initialize(text, data_path: nil, history_path: nil)
       @text = text
       @parsed = Code::Parser.parse(text)
@@ -101,22 +77,6 @@ class Code
         else
           abort "#{verb} not supported"
         end
-      end
-    end
-
-    private
-
-    def translate_quantity(quantity)
-      return quantity if quantity =~ /^[0-9]+$/
-
-      line = LITTERALS.detect do |litterals|
-        litterals[1..-2].include?(quantity)
-      end
-
-      if line
-        return line[-1]
-      else
-        abort "#{quantity} not supported"
       end
     end
   end
