73f34066b51c93eb0e892a813b0ad614dd46b4b4
nobody <root@localhost>
2019-09-21 12:48:55 +0200

    switch from donner to almost working define

--- test.js
+++ test.js
@@ -1,36 +1,39 @@
 #!/usr/bin/env node
 
-Parser = require("./lib/code/parser.js").Parser
 process = require("process")
 
-samples = [
-  "give 1",
-  "help",
-  "exit",
-  "show",
-  "give 10 to dorian michel marié",
-  "give 0 to angélique from dorian",
-  "give 3 patates to laurie marié from dorian marié for l'eau",
-  "give three verres d'eau to damien",
-  "define a person as un objet",
-  "define show as show; show"
+let Parser = require("./lib/code/parser.js").Parser
+
+let samples = [
+//  "give 1",
+//  "help",
+//  "exit",
+//  "show",
+//  "give 10 to dorian michel marié",
+//  "give 0 to angélique from dorian",
+//  "give 3 potatoes to laurie marié from dorian marié for the water",
+//  "give three glasses of water to damien",
+//  "define a person as an object",
+`define show a string as (in ruby)
+  puts string
+(end of ruby)
+define a person as an object`
 ]
 
 for (sample of samples) {
-  console.log("\n### " + sample + "\n")
   try {
     tree = Parser.parse(sample)
     console.log(JSON.stringify(tree, null, 2))
   } catch(e) {
-    console.log(e.message)
     console.log(sample)
-    offset = e.location.start.offset
+    column = e.location.start.column
     arrow = ""
-    for (i = 1; i < offset; i += 1) {
+    for (i = 0; i < column; i += 1) {
       arrow = arrow + "-"
      }
     console.log(arrow + '^')
-    console.log(JSON.stringify(e, null, 2))
+    console.log(`line ${e.location.start.line}`)
+    console.log(e.message)
     process.exit(0)
   }
 }
